97f0ba5b711c25ebf46cae098ae7cbc56cb87e9e
Parth <parthashara58@gmail.com>
2026-07-20 15:22:20 +0530

    Revert product selection to radio buttons

--- pages/blog/index.jsx
+++
@@ -1,8 +0,0 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-
-export default function BlogRedirect() {
-  const router = useRouter();
-  useEffect(() => { router.replace('/blogs'); }, []);
-  return null;
-}
--- pages/ping.js
+++
@@ -1,1 +0,0 @@
-export default function Ping() { return <p>ok</p>; }
